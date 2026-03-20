2395c033ddb94eaa0e7b7310e2888d6bef7713c7
EmmanuelleCloutier <darkforest1045@gmail.com>
2026-03-20 13:48:17 -0400

    start API meteo + updated readme

---
+++ MeteoVR/Assets/Scripts/GetCurrentWeatherInfo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetCurrentWeatherInfo: MonoBehaviour
+{
+    /*
+    const string URL_GetPublicIP="https://api.ipify.org/";
+
+    IEnumerator GetWeatherPhase1PublicIP()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(URL_GetPublicIP))
+        {
+            request.timeout = 1;
+            yield return request.sendWebRequest();
+            
+        }
+        
+        yield return null;
+    }*/
+}
